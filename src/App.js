7184cc416ad1408ddc8521420fb595b4841a1269
aloha-fim <frederec.im@gmail.com>
2023-04-13 20:46:47 -1000

    fetching data with non-implementing token

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
 
-  const [movies, setMovies] = useState(['Movie 1', "Movie 2"]);
+  const [movies, setMovie] = useState([]);
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/api/movies", {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Token'
+      }
+    })
+    .then( resp => resp.json())
+    .then( resp => setMovie(resp))
+    .catch( error => console.log(error))
+  }, [])
 
   return (
     <div className="App">
@@ -13,7 +26,7 @@ function App() {
       <div className="layout">
           <div>
             { movies.map( movie => {
-              return <h2>{movie}</h2>
+              return <h2>{movie.title}</h2>
             })}
           </div>
           <div>Movie details</div>
