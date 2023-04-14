d6b74281860671f3aa4f3833aea6e6c41a5711e9
aloha-fim <frederec.im@gmail.com>
2023-04-13 20:08:59 -1000

    add list component and change file structure into repo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import logo from './logo.svg';
-
+import React, { useState } from 'react';
+import './App.css';
 
 function App() {
+
+  const [movies, setMovies] = useState(['Movie 1', "Movie 2"]);
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Mover rater</h1>
       </header>
       <div className="layout">
-          <div>Movie list</div>
+          <div>
+            { movies.map( movie => {
+              return <h2>{movie}</h2>
+            })}
+          </div>
           <div>Movie details</div>
         </div>
     </div>
