86c6081be8805c70651de7cf81feee259453b7bd
aloha-fim <frederec.im@gmail.com>
2023-04-14 20:31:34 -1000

    edit components

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import MovieList from './components/movie-list';
 import MovieDetails from './components/movie-details';
+import MovieForm from './components/movie-form';
 
 function App() {
 
   const [movies, setMovie] = useState([]);
   const [selectedMovie, setSelectedMovie] = useState(null);
+  const [editedMovie, setEditedMovie] = useState(null);
 
   useEffect(() => {
     fetch("http://127.0.0.1:8000/api/movies", {
@@ -27,7 +29,10 @@ function App() {
   }
 
   const loadMovie = movie => {
-    selectedMovie(movie);
+    setSelectedMovie(movie);
+  }
+  const editClicked = movie => {
+    setEditedMovie(movie);
   }
 
   return (
@@ -36,8 +41,9 @@ function App() {
         <h1>Mover rater</h1>
       </header>
       <div className="layout">
-          <MovieList movies={movies} movieClicked={movieClicked}/>
+          <MovieList movies={movies} movieClicked={movieClicked} editClicked={editClicked}/>
           <MovieDetails movie={selectedMovie} updateMovie={loadMovie}/>
+          <MovieForm movie={editedMovie}/>
         </div>
     </div>
   );
--- src/components/movie-list.js
+++ src/components/movie-list.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEdit } from '@fortawesome/free-solid-svg-icons'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
 
 function MovieList(props){
 
     const movieClicked = movie => evt => {
         props.movieClicked(movie)
 
+    }
+    const editClicked = movie => {
+        props.editClicked(movie);
     }
 
     return (
@@ -13,6 +19,8 @@ function MovieList(props){
               return (
                 <div key={movie.id}>
                     <h2 onClick={movieClicked(movie)}>{movie.title}</h2>
+                    <FontAwesomeIcon icon={faEdit} onClick={() => editClicked()}/>
+                    <FontAwesomeIcon icon={faTrash} />
                 </div>
               )
             })}
