dd9b65aebc475be2471fcdfe55dc9f6953048378
aloha-fim <frederec.im@gmail.com>
2023-04-15 02:35:01 -1000

    refactor components with api service file

---
+++ src/api-service.js
@@ -0,0 +1,14 @@
+const TOKEN = "token";
+
+export class API {
+    static updateMovie(mov_id, body) {
+        return fetch(`http://127.0.0.1:8000/api/movies/${mov_id}/`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Token ${TOKEN}`
+            },
+            body: JSON.stringify( body )
+        }).then( resp => resp.json())     
+    }
+}
