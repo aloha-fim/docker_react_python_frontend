b59668d0c7aa326e7bfab862c1b86c84cfd59e01
aloha-fim <frederec.im@gmail.com>
2023-04-15 03:50:31 -1000

    adding reactbrowser to route

--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Auth from './components/auth';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from './serviceWorker';
+import { Route, BrowserRouter} from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Route exact path="/" component={Auth} />
+      <Route exact path="/movies" component={App} />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
