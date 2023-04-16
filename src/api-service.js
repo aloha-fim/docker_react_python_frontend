1fde7918eb98a2f03390654a0b3a9197ab7f149d
aloha-fim <frederec.im@gmail.com>
2023-04-16 05:38:41 -1000

    setup auth api service for login

--- src/api-service.js
+++ src/api-service.js
@@ -1,6 +1,15 @@
 const TOKEN = "token";
 
 export class API {
+    static loginUser(body) {
+        return fetch(`http://127.0.0.1:8000/register/`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify( body )
+        }).then( resp => resp.json())     
+    }
     static updateMovie(mov_id, body) {
         return fetch(`http://127.0.0.1:8000/api/movies/${mov_id}/`, {
             method: 'PUT',
