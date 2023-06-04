1290565eafdaa3981fcb67d8a755d22f89b9c0b1
aloha-fim <frederec.im@gmail.com>
2023-06-03 21:16:12 -1000

    add feature: index navbar

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,17 +1,27 @@
 import { Outlet } from "react-router-dom";
 
+import IndexNavbar from "./components/indexnavbar/IndexNavbar";
 import Header from "./components/header/header";
 
-const Layout = () => {
+export default function Layout() {
+    React.useEffect(() => {
+      document.body.classList.toggle("index-page");
+      // Specify how to clean up after this effect:
+      return function cleanup() {
+        document.body.classList.toggle("index-page");
+      };
+    }, []);
+
     return (
         <>
         {/* put header here */}
+        <IndexNavbar />
         <div className="wrapper">
-            <Header />
-            <Outlet />
+          <Header />
+          <Outlet />
         </div>
         </>
     );
 };
 
-export default Layout;
+
